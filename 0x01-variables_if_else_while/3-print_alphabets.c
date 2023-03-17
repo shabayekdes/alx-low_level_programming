8e3bea2af030c6a05707d507e34b271fddf15d68
Esmail Shabayek <esmail.shabayek@gmail.com>
2023-03-18 01:13:50 +0200

    Fix print alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ *
+ * Return: Always 0 (Sucess)
  */
+
 int main(void)
 {
-	char low;
+	char c;
 
-	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
-	for (low = 'A'; low <= 'Z'; low++)
-		putchar(low);
-		putchar('\n');
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		putchar(c);
+	}
 
+	for (c = 'A' ; c <= 'Z' ; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
 	return (0);
 }
