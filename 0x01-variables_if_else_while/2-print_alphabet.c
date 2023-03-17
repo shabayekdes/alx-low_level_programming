33c71a74ac2f63ff4472aea4d3b342700295dd61
Esmail Shabayek <esmail.shabayek@gmail.com>
2023-03-18 01:11:55 +0200

    Fix print alphabet file

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ *
+ * Return: Always 0 (Sucess)
  */
+
 int main(void)
 {
-	char low;
+	char c;
 
-	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
-		printf("\n");
-
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
 	return (0);
 }
