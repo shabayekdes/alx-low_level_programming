a4ccdc9cc24ec8a1e0284edc24ef527ce28e1417
Esmail Shabayek <esmail.shabayek@gmail.com>
2023-05-01 01:01:17 +0300

    Add 4. Free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a linked list
+ * @head: listint_t list to be freed
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp_node;
+
+	while (head)
+	{
+		temp_node = head->next;
+		head = temp_node;
+		free(head);
+	}
+}
