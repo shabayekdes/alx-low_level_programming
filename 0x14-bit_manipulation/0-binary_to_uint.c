2515ad4657f3498a00d0d6775a18d27790c18b7d
Esmail Shabayek <esmail.shabayek@gmail.com>
2023-05-05 02:13:16 +0300

    Fix task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,17 +8,23 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int result = 0;
-    int i;
+	unsigned int total, power;
+	int i;
 
-    if (b == NULL)
-        return 0;
+	if (b == NULL)
+		return (0);
 
-    for (i = 0; b[i] != '\0'; i++) {
-        if (b[i] < '0' || b[i] > '1')
-            return 0;
-        result = (result << 1) + (b[i] - '0');
-    }
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
 
-    return (result);
+	for (power = 1, total = 0, i--; i >= 0; i--, power *= 2)
+	{
+		if (b[i] == '1')
+			total += power;
+	}
+
+	return (total);
 }
