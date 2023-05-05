0fa96c92260bdf5623c4723d44f5346225e1148d
Esmail Shabayek <esmail.shabayek@gmail.com>
2023-05-05 03:03:59 +0300

    Add task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * flip_bits - flip bits to convert one number to another number
+ * @n: first number
+ * @m: second number to convert to
+ * Return: number of bits that was needed to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int count = 0;
+	unsigned long int xor_result = n ^ m;
+
+	while (xor_result > 0)
+	{
+		count += xor_result & 1;
+		xor_result >>= 1;
+	}
+
+	return (count);
+}
